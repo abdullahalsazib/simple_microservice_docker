4a381b9cb99153c366fac82a4672bd00202a48e2
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-01-08 19:54:18 +0600

    8_Jan_25|7:54PM

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	router.HandleFunc("/message", hndlr.GetMessageHandler).Methods("GET")
 	router.HandleFunc("/greet/{name}", hndlr.DynamicHandler).Methods("GET")
 
-	// Start the server
+	// Start the server ==>> <| <<==
 	log.Println("Starting server on :8080...")
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal(err)
